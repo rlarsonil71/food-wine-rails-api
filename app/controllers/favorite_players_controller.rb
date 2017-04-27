efba650ae3fdd13a5555504c3d841f8905afb7b8
rlarsonil71 <ron.larson@libertymutual.com>
2017-04-27 16:29:49 -0400

    Added create method for POST /favorite_players and test CURL scripts
    Added create method for POST /favorite_players within controller for
    favorite_players resource.
    
    Added new test CURL scripts in scripts/favorite_players directory copied
    from doctors CURL scripts within rails-api-clinic-code-along repo which
    test the following:
      - Create (POST /favorite_players)
      - Index (GET /favorite_players)
      - Show (GET /favorite_players/:id)
      - Update (PATCH /favorite_players/:id)
      - Delete (DESTROY /favorite_players/:id)
    
    Successfully tested CREATE (POST /favorite_players) CURL script using
    http://localhost:4741/favorite_players (returned 201 Created status).

--- app/controllers/favorite_players_controller.rb
+++ app/controllers/favorite_players_controller.rb
@@ -1,10 +1,37 @@
 # frozen_string_literal: true
 
-# GET /favorite_players
 class FavoritePlayersController < ApplicationController
+  # GET /favorite_players
   def index
     @favorite_players = FavoritePlayer.all
 
     render json: @favorite_players
+  end
+
+  # POST /favorite_players
+  def create
+    @favorite_player = FavoritePlayer.new(favorite_player_params)
+
+    if @favorite_player.save
+      render json: @favorite_player, status: :created,
+             location: @favorite_player
+    else
+      render json: @favorite_player.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # favorite_players resource
+  #   t.string :player_name
+  #   t.string :team_name
+  #   t.string :sport
+  #   t.string :position
+  #   t.integer :player_number
+
+  # Only allow a trusted parameter "white list" through.
+  def favorite_player_params
+    params.require(:favorite_player).permit(:player_name, :team_name, :sport,
+                                            :position, :player_number)
   end
 end
---
+++ scripts/favorite_players/delete-favorite_player.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# favorite_players resource
+#   t.string :player_name
+#   t.string :team_name
+#   t.string :sport
+#   t.string :position
+#   t.integer :player_number
+
+curl --include --request DELETE http://localhost:4741/favorite_players/3
+
+echo
